bcce5b4d63beb440704233ffc0f0543d825b2917
Bradley Saul <bradleysaul@fastmail.com>
2026-01-06 12:59:36 -0500

    refactor: remove commented-out configuration examples and unused settings

--- system/configuration.nix
+++ system/configuration.nix
@@ -40,26 +40,16 @@
   # Set your time zone.
   time.timeZone = "America/New_York";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkbOptions in tty.
-  # };
+
  
   # Enable fingerprint support
   services.fprintd.enable = true;
 
-  # services.picom.enable = true;
-  # dbus = {
-  #   enable = true;
-  #   socketActivated = true;
-  # };
+
   # Enable touchpad support.
   services.libinput.enable = true;
 
@@ -90,19 +80,9 @@
     # };
   };
   
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = {
-  #   "eurosign:e";
-  #   "caps:escape" # map caps to escape.
-  # };
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
 
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
+
 
   security.rtkit.enable = true;
 
@@ -132,12 +112,7 @@
   environment.sessionVariables.TERMINAL = ["kitty"];
 
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.gnupg.agent = {
-  #    enable = true;
-  #    enableSSHSupport = true;
-  # };
+
 
   programs = {
     zsh.enable = true;
@@ -153,14 +128,9 @@
   # List services that you want to enable:
 
   services.gnome.gnome-keyring.enable = true;
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+
+
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
