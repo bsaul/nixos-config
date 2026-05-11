9fc37c665aa7ebdfb24855bd3fe9a82c91566210
Bradley Saul <bradleysaul@fastmail.com>
2026-05-11 17:35:23 -0400

    change vscode settings

--- system/flake.nix
+++ system/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
